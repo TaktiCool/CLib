e727982a784ca90a7de71b6bdd91e859fcfcfeca
jokoho482 <hoffmann.jonas95@gmail.com>
2016-10-24 01:49:14 +0200

    push Version Number

--- addons/CLib/CLib_Macros.hpp
+++ addons/CLib/CLib_Macros.hpp
@@ -3,16 +3,16 @@
 #define MOD CLib
 
 #define MAJOR 0
-#define MINOR 1
+#define MINOR 2
 #define PATCHLVL 0
-#define BUILD 5
+#define BUILD 150
 
 // dont release with this setting enabled
 // #define DEBUGFULL // enable all Debug Methods
-// #define isDev // enable better logging
+#define isDev // enable better logging
 // #define ENABLEPERFORMANCECOUNTER // enable Performance counter for Function calls
 // #define ENABLEFUNCTIONTRACE // enable SQF based Function Tracer (Later Maybe Replace with ChromeTrace Repalced)
-#define disableCompression
+// #define disableCompression
 // Base Includes
 #include "macros.hpp"
 #include "InternalMacros.hpp"
