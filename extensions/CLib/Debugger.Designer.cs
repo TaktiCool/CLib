ec2da4d97f2e825bd1c432803a2bb76491d379ca
NetFusion <terrorhund.the.chilla@hotmail.de>
2016-11-23 23:07:27 +0100

    dll update

---
+++ extensions/CLib/Debugger.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace CLib
+{
+    partial class Debugger
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtb_log = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // rtb_log
+            // 
+            this.rtb_log.BackColor = System.Drawing.Color.Black;
+            this.rtb_log.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtb_log.ForeColor = System.Drawing.Color.Lime;
+            this.rtb_log.Location = new System.Drawing.Point(12, 12);
+            this.rtb_log.Name = "rtb_log";
+            this.rtb_log.Size = new System.Drawing.Size(639, 237);
+            this.rtb_log.TabIndex = 0;
+            this.rtb_log.Text = "";
+            this.rtb_log.TextChanged += new System.EventHandler(this.rtb_log_TextChanged);
+            // 
+            // Debugger
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(663, 261);
+            this.Controls.Add(this.rtb_log);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Debugger";
+            this.Text = "Debugger";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtb_log;
+    }
+}
--- extensions/CLib/DllEntry.cs
+++ extensions/CLib/DllEntry.cs
@@ -1,6 +1,7 @@
 ﻿using RGiesecke.DllExport;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -8,70 +9,64 @@ namespace CLib
 {
     public class DllEntry
     {
-        private static Dictionary<string, Delegate> voidActions = new Dictionary<string, Delegate>();
-        private static Dictionary<string, Func<string, string>> dataActions = new Dictionary<string, Func<string, string>>();
-
-        public static string startTime;
-        private static string fromGameData = "";
-        private static string toGameData = "";
-        private static List<string> toGameDataSplit;
+        public static Debugger Debugger;
+        private static List<Guid> tasks = new List<Guid>();
 
         static DllEntry()
         {
-            startTime = Misc.currentDate("{0}-{1}-{2}_{3}-{4}-{5}");
+            DllEntry.Debugger = new Debugger();
+            DllEntry.Debugger.Show();
+            DllEntry.Debugger.Log("Console created.");
         }
 
         [DllExport("_RVExtension@12", CallingConvention = System.Runtime.InteropServices.CallingConvention.Winapi)]
         public static void RVExtension(StringBuilder output, int outputSize, [MarshalAs(UnmanagedType.LPStr)] string input)
         {
-            output.Append("Done");
-            switch (input)
-            {
-                case "frameworkClear":
-                    fromGameData = "";
-                    toGameData = "";
-                    break;
-                case "frameworkImport":
-                    if (toGameDataSplit.Count != 0)
-                    {
-                        output.Clear();
-                        output.Append(toGameDataSplit[0]);
-                        toGameDataSplit.RemoveAt(0);
-                    }
-                    break;
-                default:
-                    string[] inputSplited = input.Split(new char[] { ':' }, 2);
-                    if (voidActions.ContainsKey(inputSplited[0]))
-                    {
-                        voidActions[inputSplited[0]]();
-                    }
-                    else if (dataActions.ContainsKey(inputSplited[0]))
-                    {
-                        string tempData = dataActions[input](fromGameData);
-                        if (tempData != "")
-                        {
-                            toGameDataSplit = splitToGameDataArray(tempData);
-                        }
-                    }
-                    else
-                    {
-                        fromGameData += input;
-                    }
-                    break;
-            }
-        }
-        public static List<string> splitToGameDataArray(string input)
-        {
+            if (input == "")
+                return;
 
-            int chunkSize = 7000;
-            int stringLength = input.Length;
-            List<string> stringArray = new List<string>();
-            for (int i = 0; i < stringLength; i += chunkSize)
+            if  (input[0] == '>')
             {
-                if (i + chunkSize > stringLength) chunkSize = stringLength - i;
-                stringArray.Add(input.Substring(i, chunkSize));
+                try
+                {
+                    string[] parameter = input.Substring(1).Split(new char[] { '|' }, 3);
+                    if (parameter.Length < 2)
+                        return;
+                    output.Append(DllEntry.Execute(parameter[0], parameter[1]));
+                }
+                catch (Exception e)
+                {
+                    output.Append(e.Message);
+                }
             }
-            return stringArray;
+
+            if (input[0] == '<')
+            {
+                try
+                {
+                    if (DllEntry.tasks.Count == 0)
+                        return;
+
+                    Guid id = DllEntry.tasks[0];
+                    DllEntry.tasks.RemoveAt(0);
+                    output.Append(id);
+                }
+                catch (Exception e)
+                {
+                    output.Append(e.Message);
+                }
+            }
+
+            outputSize--;
+        }
+
+        private static string Execute(string extension, string action)
+        {
+            Guid id = Guid.NewGuid();
+
+            DllEntry.tasks.Add(id);
+
+            return id.ToString();
         }
     }
 }
