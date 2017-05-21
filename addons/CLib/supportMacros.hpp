d66d744685d58fc89682d23468edd55a62ffc85f
jokoho48 <hoffmann.jonas95@gmail.com>
2017-05-21 15:32:08 +0200

    use new Vanilla Function/command for Admin/loggedAdmin Detection

--- addons/CLib/supportMacros.hpp
+++ addons/CLib/supportMacros.hpp
@@ -22,9 +22,9 @@
 #define IS_NUMBER(var)   IS_SCALAR(var)
 
 // is Voted Admin Server (not 100% supported the Server Owner can change the comands what the voted Admin can use)
-#define IS_ADMIN serverCommandAvailable "#kick"
+#define IS_ADMIN ((call BIS_fnc_admin) isEqualTo 1)
 // is Logged Admin Server (not 100% supported the Server Owner can change the comands what the voted Admin can use)
-#define IS_ADMIN_LOGGED serverCommandAvailable "#shutdown"
+#define IS_ADMIN_LOGGED ((call BIS_fnc_admin) isEqualTo 2)
 
 // Deprecated System for Functions
 #define DEPRECATEFUNC(OLD_FUNC,NEW_FUNC) \
